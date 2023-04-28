4b1842b7ae70feac59ebf6feebc17735a5bd31e7
Alex Delong <alexdelong243@gmail.com>
2023-04-28 16:46:37 -0700

    expanded and commented the interface

--- include/libneuron.hpp
+++ include/libneuron.hpp
@@ -1,23 +1,45 @@
 class Edge;
 class Neuron;
-
-// a connection to a neuron
-class Edge {
-    Neuron* tip;
-    float weight;
-public:
-    // contructor to connect to a neuron
-    Edge(Neuron*);
-    // transmit signal to tip
-    void transmit(float);
-};
-
-// 
-class Neuron {
-    Edge* edges;
-public:
-    // constructor to connect to an array of neurons
-    Neuron(Neuron*);
-    // a non-linear function for recieving inputs
-    void activate();
-};
+namespace LibNeuron {
+    // a connection to a neuron
+    class Edge {
+        // the neuron recieving the signal 
+        Neuron* tip;
+        // the coefficient of the signal
+        float weight;
+    public:
+        // contructor to connect to a Neuron
+        // pre-conditions:
+            // argument points to a valid Neuron
+        // post-conditions:
+            // tip == argument
+        Edge(Neuron*);
+        // weight setter
+        void set_weight(float);
+        // sends the weighted argument signal to the tip Neuron
+        void transmit(float);
+        ~Edge();
+    };
+ 
+    class Neuron {
+        // the unprocessed sum of the input signals
+        float input_signal;
+        // an array of output Edges
+        Edge* output_edges;
+    public:
+        // constructor to connect to an array of neurons
+        Neuron(Neuron*);
+        // add the argument to the input signal
+        void signal_add(float);
+        // a non-linear function that processes the signal
+        float activation();
+        // a function to send the processed signal to output edges
+        // pre-conditions:
+            // all input edges have passed their signals
+        // post-conditions:
+            // this has transmitted its processed signal to the output edges
+            // input_signal == 0
+        void fire();
+        ~Neuron();
+    };
+}
