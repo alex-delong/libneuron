e0c29dc692d6073941414cc7043422395913ae27
Alex Delong <alexdelong243@gmail.com>
2023-05-05 16:34:37 -0700

    put functions into namespace

--- include/libbinfuns.hpp
+++ include/libbinfuns.hpp
@@ -1,6 +1,8 @@
-bool is_odd(int i);
-void init_bin(bool* bin_arr);
-//  post-conditions: 
-//      returns an array of bools that represent the argument in binary  
-bool* to_bin(unsigned int i); 
-unsigned int to_int(bool* bin_arr);
+namespace BinFuns {
+    bool is_odd(int i);
+    void init_bin(bool* bin_arr);
+    //  post-conditions: 
+    //      returns an array of bools that represent the argument in binary  
+    bool* to_bin(unsigned int i); 
+    unsigned int to_int(bool* bin_arr);
+};
