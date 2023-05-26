a66746798237fef874766059977ca4db2950de9c
Alex Delong <alexander.delong@proton.me>
2023-05-26 16:01:09 -0700

    fix: remove dynamic linking macro and main definition from neuron module and move them to main.cpp

--- test/neuron.cpp
+++ test/neuron.cpp
@@ -1,6 +1,4 @@
 #define BOOST_TEST_MODULE Neuron test
-#define BOOST_TEST_NO_MAIN
-#define BOOST_TEST_DYN_LINK
 
 #include <network.hpp>
 #include <neuron.hpp>
@@ -142,7 +140,3 @@ BOOST_AUTO_TEST_CASE(fire) {
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
-int main(int argc, char** argv) {
-    return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
-}
